8e73e2584022c112fdddd2f48ebe58be5912c19c
OSKARHAGMAN <93186342+OSKARHAGMAN@users.noreply.github.com>
2022-09-12 14:54:30 +0200

    Initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,44 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class test {
+    public static WebDriver driver;
+    public static String URL = "https://maven.apache.org/";
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    @BeforeAll
+    public static void launchBoostAppHomepage() {
+        System.out.println("Launch Chrome Browser");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1100,1000");
+        options.setHeadless(true);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.get(URL);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    @Test
+    void mvnTest(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("breadcrumbs")));
+        System.out.println("Element is clickable");
+        String elementText = driver.findElement(By.id("breadcrumbs")).getText();
+        System.out.println(elementText);
+    }
+    @AfterAll
+    static void tearDown() {
+        driver.quit();
+    }
+}
+
